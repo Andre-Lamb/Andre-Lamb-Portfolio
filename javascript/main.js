81839c18442aecd47db63ef67ff30f3647d63575
Andre-Lamb <162134520+Andre-Lamb@users.noreply.github.com>
2026-08-06 15:11:14 +1000

    JS!

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,26 @@
-$(document).ready(function(){
+//Mobile ribbon variables
+const ribbonMotion = [{transform: 'translateY(-20px)'}, {transform: 'translateY(100px)'},];
+const ribbonTime = {duration: 100, iterations: 1,};
+const ribbon = document.querySelector('a');
 
-    const theNav = $('header nav')[0];
-    const theBurger = $('header nav a')[0];
-    let burgerOpen = Boolean(false); //sets the default state of the nav
+ribbon.onclick = function() {
+    ribbon.animate(ribbonMotion, ribbonTime);
+    ribbon.transform = 'translateY(100)';
+}
+//Breakdown variables
+const breakdownText = document.querySelectorAll('.breakdown-text p');
+const breakdownImages = document.querySelector('.scroll-container');
 
-    theBurger.onclick = function(){
-    theNav.classList.toggle('open-nav');
-    burgerOpen = !burgerOpen; //sets the current state of the nav
-    };
-
-    window.onscroll = function(){ //closes the nav if the user scrolls on the page
-        if (burgerOpen) { //checks if the nav is open
-            theNav.classList.toggle('open-nav');
-            burgerOpen = !burgerOpen;
-        };
-    };
-
-    const theParagraphs = $('#lineup .artists-text p');
-    const theImgs = $('#lineup .artists-imgs img');
-
-    function onResize(){
-        if (screen.width<=480) { //only runs this code for mobile sizes
-            for (let index = 0; index < theParagraphs.length; index++) {
-                const element = theParagraphs[index];
-                let theHeight = $(element).height(); //gets the paragraphs height
-                let theOffset = $(theImgs[index]).height(); //gets the images height
-                let theMargin = theHeight + 6 - theOffset; //calculates the difference accounting for margin
-                $(theImgs[index]).css('margin-bottom', theMargin); //sets the images bottom margin to the difference
-            }
-        }else{
-            $(theImgs).css('margin-bottom', 0); //removes the margin if moving from a phone scale to tablet scale, such as when rotating screen
-        };
-    };
-
-    window.addEventListener('resize', onResize);
-
-});
+function breakdownUpdate(){
+    let scrollOffset = breakdownImages.scrollWidth*0.05;
+    let scrollAmt = (breakdownImages.scrollLeft+scrollOffset)/(breakdownImages.scrollWidth-window.innerWidth);
+    scrollAmt = Math.floor((breakdownText.length-1)*scrollAmt);
+    console.log(scrollAmt)
+    if (scrollAmt<0) scrollAmt = 0;
+    for (let index = 0; index < breakdownText.length; index++) {
+        if (index == scrollAmt) breakdownText[index].style.display = 'inline';
+        else breakdownText[index].style.display = 'none';
+    }
+};
+window.addEventListener('load', breakdownUpdate);
+breakdownImages.addEventListener('scroll', breakdownUpdate);
