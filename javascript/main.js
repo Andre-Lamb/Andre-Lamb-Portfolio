04d38e5b512c4c54c4bbae066266dafb6dc5d369
Andre-Lamb <lambdre2001@gmail.com>
2026-08-12 00:22:17 +1000

    Almost done with the framework

--- javascript/main.js
+++ javascript/main.js
@@ -45,42 +45,71 @@
 //Mobile ribbon variables
 const ribbon = document.querySelector('.mobile-flag');
 let ribbonDown = false;
-
+//Toggles the mobile ribbon on and off
 ribbon.onclick = function() {
     ribbon.classList.toggle('flag-down');
     ribbonDown = !ribbonDown;
 }
 //Breakdown variables
+const breakdownTitle = document.querySelectorAll('.breakdown-text h3');
 const breakdownText = document.querySelectorAll('.breakdown-text p');
-const breakdownImages = document.querySelector('.scroll-container');
-
+const breakdownImageDiv = document.querySelector('.scroll-container');
+const breakdownImageArray = document.querySelectorAll('.scroll-container img');
+let breakdownBigBool = false;
+let breakdownBigId = -1;
+//Changes the text corresponding to breakdown scroll
 function breakdownUpdate(){
-    let scrollOffset = breakdownImages.scrollWidth*0.05;
-    let scrollAmt = (breakdownImages.scrollLeft+scrollOffset)/(breakdownImages.scrollWidth-window.innerWidth);
+    let scrollOffset = breakdownImageDiv.scrollWidth*0.05;
+    let scrollAmt = (breakdownImageDiv.scrollLeft+scrollOffset)/(breakdownImageDiv.scrollWidth-window.innerWidth);
     scrollAmt = Math.floor((breakdownText.length-1)*scrollAmt);
     if (scrollAmt<0) scrollAmt = 0;
     for (let index = 0; index < breakdownText.length; index++) {
-        if (index == scrollAmt) breakdownText[index].style.display = 'inline';
-        else breakdownText[index].style.display = 'none';
+        if (index == scrollAmt){
+            breakdownTitle[index].style.display = 'block';
+            breakdownText[index].style.display = 'block';
+        }
+        else{
+            breakdownTitle[index].style.display = 'none';
+            breakdownText[index].style.display = 'none';
+        }
     }
 };
-
+//Makes an image expand or shrink
+function breakdownImg(i) {
+    if (breakdownBigBool) {
+        if (i == breakdownBigId) {
+            breakdownImageArray[i].classList.toggle('clicked-img');
+            breakdownBigBool = false;
+        }
+        return;
+    }
+    breakdownImageArray[i].classList.toggle('clicked-img');
+    breakdownBigId = i;
+    breakdownBigBool = true;
+}
+for (let i = 0; i < breakdownImageArray.length; i++) {
+    breakdownImageArray[i].addEventListener('click', function() {
+    breakdownImg(i);
+});
+}
+//Rolling variables
 const header = document.querySelector('#rolling');
-
+//Makes the header background roll as the user scrolls the page
 document.onscroll = function() {
     //Moving the header's texture to mimic scrolling
     let scrollCurrent = document.documentElement.scrollTop/(document.documentElement.scrollHeight-window.innerHeight);
     scrollCurrent = 100-100*scrollCurrent + '%';
     scrollCurrent = '50% ' + scrollCurrent;
-    console.log(scrollCurrent);
     header.style.backgroundPosition = scrollCurrent;
-    console.log(header.style.backgroundPosition);
     //Closing the burger menu if open
     if (ribbonDown) {
         ribbon.classList.toggle('flag-down');
         ribbonDown = false;
     }
+    if (breakdownBigBool) {
+        breakdownImg(breakdownBigId);
+    }
 }
-
+//Listeners
 window.addEventListener('load', breakdownUpdate);
-breakdownImages.addEventListener('scroll', breakdownUpdate);
+breakdownImageDiv.addEventListener('scroll', breakdownUpdate);
